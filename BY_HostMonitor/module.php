2b56320e499d7f65835034643cb80a7727a9c0b1
BayaroX <github_com@heaven-server.de>
2015-12-24 17:14:01 +0100

    Fix // Instanzen jetzt korrekt als Integer registriert

--- BY_HostMonitor/module.php
+++ BY_HostMonitor/module.php
@@ -16,9 +16,9 @@ class HostMonitor extends IPSModule
         $this->RegisterPropertyInteger("AlarmZeitDiff", 0);
         $this->RegisterPropertyString("BenachrichtigungsTextOffline", "Der Host -§HOST- mit Adresse -§ADRESSE- ist seit §ZEITMIN Minuten nicht mehr erreichbar!");
         $this->RegisterPropertyString("BenachrichtigungsTextOnline", "Der Host -§HOST- mit Adresse -§ADRESSE- war §ZEITMIN Minuten offline und ist jetzt wieder erreichbar!");
-        $this->RegisterPropertyString("WebFrontInstanceID", "");
-        $this->RegisterPropertyString("SmtpInstanceID", "");
-        $this->RegisterPropertyString("EigenesSkriptID", "");
+        $this->RegisterPropertyInteger("WebFrontInstanceID", 0);
+        $this->RegisterPropertyInteger("SmtpInstanceID", 0);
+        $this->RegisterPropertyInteger("EigenesSkriptID", 0);
         $this->RegisterPropertyBoolean("LoggingAktiv", false);
         $this->RegisterPropertyBoolean("OfflineBenachrichtigung", false);
         $this->RegisterPropertyBoolean("OnlineBenachrichtigung", false);
